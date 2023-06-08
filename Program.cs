8b3f8c5f32387ddb6b1d861442d16a46fc6b2431
Namidachill <kicosy@gmail.com>
2023-06-08 12:26:48 +0900

    プロジェクト ファイルを追加します。

---
+++ Program.cs
@@ -0,0 +1,124 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using CharacterAI_Discord_Bot.Service;
+using CharacterAI_Discord_Bot.Handlers;
+
+namespace CharacterAI_Discord_Bot
+{
+    public class Program : CommandsService
+    {
+        private ServiceProvider _services = null!;
+        private DiscordSocketClient _client = null!;
+
+        static void Main()
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private async Task MainAsync()
+        {
+            _services = CreateServices();
+            _client = _services.GetRequiredService<DiscordSocketClient>();
+
+            if (BotConfig is null) return;
+
+            // So it would simply not show that annoying error in console.
+            // Presence Intent itself is actually needed for commands that take user as an argument.
+            _client.PresenceUpdated += (a, b, c) => Task.CompletedTask;
+            _client.Log += Log;
+            _client.Ready += OnClientReady;
+            _client.JoinedGuild += OnGuildJoin;
+
+            await _client.LoginAsync(TokenType.Bot, BotConfig.BotToken);
+            await _client.StartAsync();
+            await _services.GetRequiredService<CommandsHandler>().InitializeAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private async Task OnGuildJoin(SocketGuild guild)
+        {
+            await CreateBotRoleAsync(guild);
+            Success($"Joined guild: {guild.Name} | Owner: {guild.Owner.Username} | Members: {guild.MemberCount}");
+        }
+
+        public Task OnClientReady()
+        {
+            _ = Task.Run(async () => await LaunchChromeAndSetup(setup: BotConfig.AutoSetupEnabled));
+            
+            return Task.CompletedTask;
+        }
+
+        private async Task LaunchChromeAndSetup(bool setup)
+        {
+            try
+            {
+                var handler = _services.GetRequiredService<CommandsHandler>();
+                await handler.CurrentIntegration.LaunchChromeAsync(BotConfig.CustomChromePath, BotConfig.CustomChromeExecPath);
+                if (setup) await AutoSetup(handler, _client);
+
+                CommandsLog();
+                while (true)
+                {
+                    Log("\n# ", ConsoleColor.Green);
+                    string? input = Console.ReadLine();
+                    input = input?.Trim().Trim('\"');
+
+                    if (input is null) continue;
+
+                    switch (input)
+                    {
+                        case "exit" or "stop": Environment.Exit(0); break;
+                        case "kill": KillChromes(); break;
+                        case "launch": await LaunchChromeAndSetup(false); break;
+                    };
+                }
+            }
+            catch (Exception e)
+            {
+                Failure(e.ToString(), client: _client);
+            }
+        }
+
+        private static ServiceProvider CreateServices()
+        {
+            var clientConfig = new DiscordSocketConfig
+            {
+                GatewayIntents = GatewayIntents.All
+                ^ GatewayIntents.GuildScheduledEvents
+                ^ GatewayIntents.GuildInvites,
+                MessageCacheSize = 5
+            };
+
+            var services = new ServiceCollection()
+                .AddSingleton(new DiscordSocketClient(clientConfig))
+                .AddSingleton(new CommandService())
+                .AddSingleton<CommandsHandler>();
+
+            return services.BuildServiceProvider();
+        }
+
+        private Task Log(LogMessage log)
+        {
+            if (log.Exception is NullReferenceException) return Task.CompletedTask;
+
+            Console.WriteLine(log.ToString());
+
+            return Task.CompletedTask;
+        }
+
+        private void KillChromes()
+        {
+            try
+            {
+                var path = _services.GetRequiredService<CommandsHandler>().CurrentIntegration.EXEC_PATH;
+                CharacterAI.Integration.KillChromes(path);
+                Success("OK");
+            }
+            catch (Exception e)
+            {
+                Failure("Failed to kill Chrome processes.\n" + e.ToString());
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,30 @@
+# CharacterAI Discord Bot
+An unofficial Discord integration with a [CharacterAI](https://beta.character.ai/) service which you can use to add any character on your own Discord server.
+
+<div align="center">
+    
+![Logo](https://user-images.githubusercontent.com/55811932/224168501-48e81f64-9b2f-442c-a8fe-6ecab8d7aab2.png)<br>
+<!-- ![Logo](https://user-images.githubusercontent.com/55811932/226441262-8edbb834-33d5-4cd2-8fac-0bacfa6ff79b.png) -->
+
+<sup><b>If you found this project useful, the best way to thank me is to simply leave a star ⭐<br>
+[Discord Server](https://github.com/drizzle-mizzle/CharacterAI-Discord-Bot/discussions/22#discussioncomment-5502307) | Based on [CharacterAI.Net](https://github.com/drizzle-mizzle/CharacterAI.Net)</b></sup>
+</div>
+
+## 🪄Features
+- Talk with any character on your own server and change them on the fly.
+- Embedded character search.
+- Automatically sets the name and profile picture of a character.
+- Supports answers swiping <s>and image sending</s>broken.
+- Supports multiple parallel chats with the same character.
+- Random replies, audience mode and some other stuff.
+
+    ![chrome_cshYMsGIaB](https://user-images.githubusercontent.com/55811932/211129383-c7cd4ca2-ceb4-42c5-8449-bc6ce9b2d538.gif)
+    
+## 📓Documentation
+- [How to set up](https://github.com/drizzle-mizzle/CharacterAI-Discord-Bot/wiki/How-to-set-up)
+- [Commands](https://github.com/drizzle-mizzle/CharacterAI-Discord-Bot/wiki/Commands)
+- [Additional configuration](https://github.com/drizzle-mizzle/CharacterAI-Discord-Bot/wiki/Additional-configuration)
+
+## 🩼Known issues
+`System.IO.IOException: The process cannot access the file because it is being used by another process.`
+Solution: Simply relaunch the application.
